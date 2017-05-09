ea9f3fec3b1b654d0aa36062fb42a7ffa3c5ae25
Douglas Liberalesso <dliberalesso@gmail.com>
2017-05-08 23:35:25 -0300

    Implementa animações.

--- cg03.c
+++ cg03.c
@@ -5,6 +5,21 @@
 #include <GL/glut.h>
 #include <math.h>
 
+#define VEL 8
+#define DIA0 0.8
+#define DIA1 0.6
+#define DIA23 0.92
+#define NOITE0 0.4
+#define NOITE1 0.2
+#define NOITE2 0.6
+#define NOITE3 0.34
+
+float sol_x;
+double cor_ceu[] = {0.7, 0.7, DIA23, DIA23};
+double step_dia = (DIA0 - DIA1) / 91;
+float ang = 0;
+int t = 0;
+
 // Desenha gramado
 void figura1(float translate_x, float translate_y) {
     // Guarda a matriz de transformação corrente na pilha
@@ -37,13 +52,13 @@ void figura2(float translate_x, float translate_y) {
     glTranslatef(translate_x, translate_y, 0);
 
     glBegin(GL_QUADS);
-    glColor3f(0.8, 0.8, 0.92);
+    glColor3d(cor_ceu[0], cor_ceu[0], cor_ceu[2]);
     glVertex2f(300, -200);
-    glColor3f(0.34, 0.34, 0.92);
+    glColor3d(0.34, 0.34, cor_ceu[3]);
     glVertex2f(300, 200);
-    glColor3f(0.34, 0.34, 0.92);
+    glColor3d(0.34, 0.34, cor_ceu[3]);
     glVertex2f(-300, 200);
-    glColor3f(0.6, 0.6, 0.92);
+    glColor3d(cor_ceu[1], cor_ceu[1], cor_ceu[2]);
     glVertex2f(-300, -200);
     glEnd();
 
@@ -164,11 +179,17 @@ void figura6(float translate_x, float translate_y) {
     glBegin(GL_TRIANGLE_FAN);
     glColor3f(1, 1, 1);
     glVertex2d(0, 0); // centro do circulo
-    glColor3f(1, 1, 0);
+
+    if (t % 2 == 0) {
+        glColor3f(1, 1, 0);
+    } else {
+        glColor3f(0.8, 0.8, 0.8);
+    }
+
     for (i = 0; i <= triangulos; i++) {
         glVertex2d(
-                0 + (75 * cos(i * doisPI / triangulos)),
-                0 + (75 * sin(i * doisPI / triangulos))
+                0 + (60 * cos(i * doisPI / triangulos)),
+                0 + (60 * sin(i * doisPI / triangulos))
         );
     }
     glEnd();
@@ -193,14 +214,63 @@ void desenha(void) {
     figura1(0, -200); // gramado
     figura2(0, 100); // ceu
     figura3(-248, -230); // casa
-    figura4(-151, 101, 0); // helice
-    figura4(-151, 101, 120); // helice
-    figura4(-151, 101, 240); // helice
+    figura6(sol_x, 221); // sol
+    figura4(-151, 101, ang); // helice
+    figura4(-151, 101, ang + 120); // helice
+    figura4(-151, 101, ang + 240); // helice
     figura5(173, -285); // arvore
-    figura6(206, 206); // sol
 
-    //Executa os comandos OpenGL
+    // Executa os comandos OpenGL
     glFlush();
+}
+
+// Função callback chamada para gerenciar eventos de teclas especiais (F1, PgDn, ...)
+void teclasEspeciais(int key, int x, int y) {
+    if (key == GLUT_KEY_LEFT) {
+        sol_x -= VEL;
+        cor_ceu[0] -= step_dia;
+        cor_ceu[1] += step_dia;
+        ang += 20;
+        if (sol_x < -360) {
+            sol_x = 360;
+            t++;
+            if (t % 2 == 0) {
+                cor_ceu[0] = DIA0;
+                cor_ceu[1] = DIA1;
+                cor_ceu[2] = DIA23;
+                cor_ceu[3] = DIA23;
+            } else {
+                cor_ceu[0] = NOITE0;
+                cor_ceu[1] = NOITE1;
+                cor_ceu[2] = NOITE2;
+                cor_ceu[3] = NOITE3;
+            }
+        }
+    }
+
+    if (key == GLUT_KEY_RIGHT) {
+        sol_x += VEL;
+        cor_ceu[0] += step_dia;
+        cor_ceu[1] -= step_dia;
+        ang -= 20;
+        if (sol_x > 360) {
+            sol_x = -360;
+            t++;
+            if (t % 2 == 0) {
+                cor_ceu[0] = DIA1;
+                cor_ceu[1] = DIA0;
+                cor_ceu[2] = DIA23;
+                cor_ceu[3] = DIA23;
+            } else {
+                cor_ceu[0] = NOITE1;
+                cor_ceu[1] = NOITE0;
+                cor_ceu[2] = NOITE2;
+                cor_ceu[3] = NOITE3;
+            }
+        }
+    }
+
+    glutPostRedisplay();
 }
 
 // Função callback chamada para gerenciar eventos de teclas
@@ -232,6 +302,9 @@ int main(int argc, char *argv[]) {
 
     // Registra a função callback de redesenho da janela de visualização
     glutDisplayFunc(desenha);
+
+    // Registra a função callback para tratamento das teclas especiais
+    glutSpecialFunc(teclasEspeciais);
 
     // Registra a função callback para tratamento das teclas ASCII
     glutKeyboardFunc(teclado);
